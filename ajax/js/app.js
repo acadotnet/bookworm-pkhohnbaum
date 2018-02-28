b94f806ce56f84d20b3d94729427b9d2978281d0
pkhohnbaum <pkhohnbaum@gmail.com>
2018-02-27 19:09:11 -0600

    book class1

--- ajax/js/app.js
+++ ajax/js/app.js
@@ -1,6 +1,32 @@
 
 $(document).ready(function(){
 
+     $.getJSON({
+        url: "./data/books.json",
+        success:function(d){
+            console.log(d);
+
+        },
+        error: function(){
+
+        }
+    })
 
 
 });
+
+class Book{
+ constructor(id, title, author, isbn, coverUrl){
+     this.id=id;
+     this.title=title;
+     this.author=author;
+     this.isbn=isbn;
+     this.coverUrl=coverUrl;
+ } 
+
+ get id(){
+     return id;
+ }
+ get
+ 
+}
